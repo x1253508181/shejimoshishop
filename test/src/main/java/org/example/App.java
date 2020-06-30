2664320c4b5000f02424835a39a404db70d32643
Xuebin <1253508181@qq.com>
2020-06-30 14:03:20 +0800

    good2

--- test/src/main/java/org/example/App.java
+++ test/src/main/java/org/example/App.java
@@ -10,5 +10,7 @@ public class App
     {
         System.out.println( "Hello World!" );
         System.out.println("hello");
+        System.out.println("hello");
+        System.out.println("hello");
     }
 }
